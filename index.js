256f83b5bdecd844260d9420800248d007bfddea
kevin diesenberg <kdiesenb@gmail.com>
2022-11-12 16:35:14 -0500

    commit tree 682e3d26e6856f88ab911227c378e2e13c3c66ce

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+
+const submitData = function(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,//from params of function //keys with in an object
+            email//im posting this info to the server/site?? from
+        })
+    }).then((response) => response.json())
+    .then((data) => document.body.textContent = data.id).catch((message) => document.body.textContent = message);
+
+};
